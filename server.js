8e914ada3afa925a76a9ae2982f4dce663310642
Kurokaii <raihanputrapramudya06@gmail.com>
2026-05-26 10:57:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,57 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+// Ambil file index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 let players = {};
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
-    console.log("Player masuk:",socket.id);
+    console.log("Player masuk:", socket.id);
 
-    socket.on("newPlayer",(data)=>{
-
-        players[socket.id] = data;
-
-        io.emit("playerJoined",{
-            id:socket.id,
-            ...data
-        });
-
-        socket.emit("allPlayers",players);
-    });
-
-    socket.on("move",(data)=>{
+    socket.on("newPlayer", (data) => {
 
         players[socket.id] = data;
 
-        io.emit("playerMoved",{
-            id:socket.id,
+        socket.emit("allPlayers", players);
+
+        socket.broadcast.emit("playerJoined", {
+            id: socket.id,
             ...data
         });
     });
 
-    socket.on("disconnect",()=>{
+    socket.on("move", (data) => {
+
+        players[socket.id] = data;
+
+        io.emit("playerMoved", {
+            id: socket.id,
+            ...data
+        });
+    });
+
+    socket.on("disconnect", () => {
 
         delete players[socket.id];
 
-        io.emit("playerLeft",socket.id);
+        io.emit("playerLeft", socket.id);
 
         console.log("Player keluar");
     });
 });
 
-server.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
 
-    console.log("Server jalan di port 3000");
+server.listen(PORT, () => {
+    console.log("Server jalan di port " + PORT);
 });
